2365ef328f09fa6cba06f612a4a906ddf929f254
przemyslaw <przemyslaw.hejman@quesma.com>
2026-09-25 15:30:17 +0200

    Handle multiline directory-user homes in macOS discovery

--- src/packaging/macos/local-users.sh
+++ src/packaging/macos/local-users.sh
@@ -23,5 +23,7 @@ console_uid=$(/usr/bin/stat -f %u /dev/console)
 if [ "$console_uid" -ge 500 ]; then
 	console_name=$(/usr/bin/stat -f %Su /dev/console)
 	console_home=$(/usr/bin/dscl /Search -read "/Users/$console_name" NFSHomeDirectory)
-	printf '%s\t%s\n' "$console_uid" "${console_home#NFSHomeDirectory: }"
+	console_home=${console_home#NFSHomeDirectory:}
+	console_home=$(printf '%s\n' "$console_home" | /usr/bin/awk 'NF { sub(/^[[:space:]]+/, ""); print; exit }')
+	printf '%s\t%s\n' "$console_uid" "$console_home"
 fi
